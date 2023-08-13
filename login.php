d981ea74f795b88640ea268c9ad9aa46a06e0611
Ching143 <139630088+Ching143@users.noreply.github.com>
2023-08-13 14:47:11 +0800

    Add files via upload

---
+++ login.php
@@ -0,0 +1,19 @@
+<?php
+
+session_start();
+
+$stored_Email = 'princecarlajoc@gmail.com';
+$stored_PassW = 'aceID132329090002';
+
+$email = $_POST["email"];
+$password = $_POST["password"];
+
+if ($email == "$stored_Email" && $password == "$stored_PassW") {
+    header("Location: loggedin.html");
+    exit();
+} else {
+    header("Location: error.html");
+    exit();
+}
+
+?>
